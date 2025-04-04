ccb37445549fce37af4fa1406ef08d0bbd43e0b0
sherwyn47 <rodriguessherwyn47@gmail.com>
2025-04-04 12:12:22 +0530

    Setup

--- Source/TankMultiplayer2D/TankMultiplayer2D.Build.cs
+++ Source/TankMultiplayer2D/TankMultiplayer2D.Build.cs
@@ -12,12 +12,16 @@ public class TankMultiplayer2D : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PublicDependencyModuleNames.Remove("PhysicsCore");
+        PublicDependencyModuleNames.Remove("Chaos");
+        PublicDependencyModuleNames.Remove("ApexDestruction");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
